0523ef712185249c9efdd552206d0dadc41be189
yazin010 <yazinbj@gmail.com>
2025-03-09 15:28:23 +0530

    s

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('users',[App\Http\Controllers\UserController::class,'index'])->name('users');
